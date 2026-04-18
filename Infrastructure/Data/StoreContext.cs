6184b8e5bbdf16c32042a3e0e37a1216949a71b7
Eldreney <Ahmed.Yehia@raqmia.net>
2026-04-18 17:44:49 +0200

    product_config

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,12 +1,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
+using Infrastructure.config;
 
 namespace Infrastructure.Data;
 
 public class StoreContext(DbContextOptions options):DbContext(options)
 {
 
-public DbSet<Product> Products { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+    }
+
+
+public DbSet<Product> Products { get; set; } 
 
 }
